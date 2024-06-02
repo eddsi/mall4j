59d487c6598def4c5f88ca8e76fcb35af3601820
Ethan Stewart <ethanste2023@gmail.com>
2024-06-02 22:37:58 +0800

    feat: document-page polished

--- front-end/mall4m/pages/document-page/document-page.js
+++ front-end/mall4m/pages/document-page/document-page.js
@@ -1,39 +1,71 @@
-// documentUpload.js
 Page({
   data: {
-    description: '',  // 文档简介
-    keywords: '',     // 文档关键字
-    file: null        // 选择的文件
+    description: '', // 文档简介
+    keywords: '', // 文档关键字
+    file: null,
+    isChecked: false // 用户是否勾选了同意协议
   },
 
-  // 输入绑定
-  bindInput: function(e) {
+  // 处理复选框变更
+  checkboxChange: function (e) {
+    console.log('Checkbox change event:', e.detail.value);
+    // 直接判断数组中是否包含特定值（如果是单个复选框，通常使用布尔值来表示是否选中）
+    this.setData({
+      isChecked: e.detail.value.includes('agree') // 假设复选框的value设置为"agree"
+    });
+  },
+
+
+  // 绑定输入框数据
+  bindInput: function (e) {
     this.setData({
       [e.currentTarget.dataset.field]: e.detail.value
     });
   },
 
-  // 选择文件
-  chooseFile: function() {
+  // 用户选择文件
+  chooseFile: function () {
     const that = this;
     wx.chooseMessageFile({
       count: 1,
       type: 'file',
       success(res) {
-        that.setData({
-          file: res.tempFiles[0]
-        });
+        // 检查文件大小不超过20MB
+        if (res.tempFiles[0].size <= 20 * 1024 * 1024) {
+          that.setData({
+            file: res.tempFiles[0]
+          });
+        } else {
+          wx.showToast({
+            title: '文件大小不能超过20MB',
+            icon: 'none'
+          });
+        }
       }
     })
   },
 
   // 上传文件
-  uploadFile: function() {
+  uploadFile: function () {
     const that = this;
-    if (this.data.file) {
-      const { path } = this.data.file;
+    if (!that.data.isChecked) {
+      wx.showToast({
+        title: '请先勾选同意用户协议和隐私政策',
+        icon: 'none'
+      });
+      return;
+    }
+
+    if (that.data.file) {
+      wx.showToast({
+        title: '上传成功',
+        icon: 'none'
+      });
+      const {
+        path
+      } = that.data.file;
       wx.uploadFile({
-        url: config.uploadUrl, // 你的上传接口地址
+        url: 'your_upload_url', // 这里替换成你的上传URL
         filePath: path,
         name: 'file',
         formData: {
--- front-end/mall4m/utils/config.js
+++ front-end/mall4m/utils/config.js
@@ -1,6 +1,7 @@
 //可改动页面
 
 var domain = "http://cp.567657.xyz:8086"; //统一接口域名，测试环境
-
+var uploadUrl = "";
 exports.domain = domain;
+exports.uploadUrl=uploadUrl;
 
